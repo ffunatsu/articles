4f7590ca0463164dd49ffe569fb7821216f5769f
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-07-14 03:58:24 +0900

    fix: 5 recent notes after body (index)

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -10,7 +10,7 @@ export const sharedPageComponents: SharedLayout = {
     Component.ConditionalRender({
       component: Component.RecentNotes({
         title: "最近の更新",
-        limit: 10,
+        limit: 5,
         showTags: false
       }),
       condition: (page: QuartzComponentProps) => page.fileData.slug === "index" || page.fileData.slug === ""
